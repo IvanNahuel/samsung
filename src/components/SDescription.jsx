4b961ba05e433774961ddd6b67e3defb7e38f5e8
ivannahsou <ivan.souza@zippin.com.ar>
2023-11-20 14:14:41 -0300

    -Aañado la descripcion

--- src/components/SDescription.jsx
+++ src/components/SDescription.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
+
 export const SDescription = ({
   title = "",
   storage = "",
   ram = "",
   mpx = "",
 }) => {
+  const [showDescription, setShowDescription] = useState(false);
+
   return (
     <div
       className="flex flex-col space-y-4 absolute bottom-[125px]"
       style={{ transition: "1s", animation: 'move-down 1s' }}
     >
       <div className="self-center">{title}</div>
-      <div>{storage}</div>
-      <div>{ram}</div>
-      <div>{mpx}</div>
+      { showDescription &&
+      <>
+        <div>{storage}</div>
+        <div>{ram}</div>
+        <div>{mpx}</div>
+        </>
+      }
     </div>
   );
 };
